622c93bede4636c527b803c4fb0b10e1965d4bed
fbzr <fabricio.spb@gmail.com>
2020-11-16 20:21:03 -0800

    fix listSlice

--- stateManagement/slices/list.js
+++ stateManagement/slices/list.js
@@ -1,25 +1,25 @@
 import {createSlice} from '@reduxjs/toolkit';
 
 const listSlice = createSlice({
-    name: 'list',
-    initialState: [
-      {
-        id: '12312dwqdf',
-        title: 'Fabricio',
-        bought: false,
-      },
-      {
-        id: 'dsfkjnsadkjng2',
-        title: 'Dani',
-        bought: false,
-      },
-    ],
-    reducers: {
-        addItem(state, action) => {}
-        removeItem(state, action) => {}
-        toggleItem(state, action) => {}
-    }
-));
+  name: 'list',
+  initialState: [
+    {
+      id: '12312dwqdf',
+      title: 'Fabricio',
+      bought: false,
+    },
+    {
+      id: 'dsfkjnsadkjng2',
+      title: 'Dani',
+      bought: false,
+    },
+  ],
+  reducers: {
+    addItem(state, action) {},
+    removeItem(state, action) {},
+    toggleItem(state, action) {},
+  },
+});
 
 export const {addItem, removeItem, toggleItem} = listSlice.actions;
 export default listSlice.reducer;
